ce4f5de7507b40f9f37a731f9412514505c2888e
mixausmuc <zaharov@web.de>
2018-12-12 11:59:12 +0100

    Create BillRestApplicationTests.java

---
+++ BillRestApplicationTests.java
@@ -0,0 +1,18 @@
+package com.mix;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.mix.bill.BillRestApplication;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=BillRestApplication.class)
+public class BillRestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
